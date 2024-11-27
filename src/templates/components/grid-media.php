c73ba565dda8dffc523aa9af4eff9db1b2816ff7
Roman Maidan <romecom2210@gmail.com>
2024-11-27 17:02:32 +0200

    update home page

--- src/templates/components/grid-media.php
+++ src/templates/components/grid-media.php
@@ -1,12 +1,23 @@
 <?php
 $img = $args['img'] ?? false;
 $type = $args['type'];
+$video = $args['video'] ?? false;
 ?>
 
 <div class="product-item <?php echo $type ?>">
   <?php if ($img) {  ?>
     <div class="product-item-icon flex obj-cover">
       <img src="<?php echo assets($img)  ?>" alt="">
+    </div>
+  <?php } ?>
+  <?php if ($video) {  ?>
+    <button class="product-item-play flex-c" data-play-video>
+      <div class="icon icon_play"></div>
+    </button>
+    <div class="product-item-video flex obj-cover">
+      <video muted loop playsinline>
+        <source src="<?php echo $video ?>" type="video/mp4">
+      </video>
     </div>
   <?php } ?>
 </div>
