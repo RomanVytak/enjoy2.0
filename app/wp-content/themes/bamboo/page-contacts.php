74bec2e85fe8553d8fc0ad20dd70c31bdbbd536c
Roman Maidan <romecom2210@gmail.com>
2024-11-29 17:04:44 +0200

    add page contacts & add plugin contact form 7

---
+++ app/wp-content/themes/bamboo/page-contacts.php
@@ -0,0 +1,11 @@
+<?php if (!defined('ABSPATH')) exit;
+
+/**
+ * Template Name: Contacts
+ * Template Post Type: page
+ */
+
+global $tpl_assets;
+$tpl_assets = 'page-contacts';
+
+renderPage('contacts');
---
+++ src/templates/sections/contacts/content.php
@@ -0,0 +1,12 @@
+<section class="feedback">
+  <div class="container flex h-between">
+
+    <h1 class="roboto-48"><?php the_title(); ?></h1>
+
+    <div class="feedback-form">
+      <?php
+      echo do_shortcode('[contact-form-7 id="39c16fe"]');
+      ?>
+    </div>
+  </div>
+</section>
