838d6adb19f99f1f431dc7920a83635b477bb4ac
molinski <to.molinski@gmail.com>
2025-02-13 23:25:58 +0200

    new builder blocks

--- src/templates/components/grid-product.php
+++ src/templates/components/grid-product.php
@@ -1,14 +1,21 @@
 <?php
+$product_id = get_sub_field('product');
+$product = wc_get_product($product_id);
 $sale = get_sub_field('sale');
-$title = get_sub_field('title');
-$price = get_sub_field('price');
-$img = wp_get_attachment_image_url(get_sub_field('img'), 'full');
-$link = get_sub_field('link');
 $type = get_row_layout();
-$color = get_sub_field('color') ?? false;
+
+$product_cats = wp_get_post_terms( $product->get_id(), 'product_cat' );
+if ( ! empty( $product_cats ) && ! is_wp_error( $product_cats ) ) {
+    // Отримати першу категорію (якщо їх декілька)
+    $main_category = $product_cats[0]; 
+    // Вивести назву категорії
+    $product_category = $main_category->name;
+	$product_category_id = $product_cats[0]->term_id;
+}
+$color = get_field('product_color', 'product_cat_'.$product_category_id);
 ?>
 
-<a href="<?php echo $link ?>" class="product-item <?php echo $type ?> <?php echo $color ? 'on-hover' : '' ?>">
+<a href="<?php echo get_permalink($product_id); ?>" class="product-item <?php echo $type ?> <?php echo $color ? 'on-hover' : '' ?>">
   <?php if ($sale) { ?>
     <div class="product-item-sale flex-c roboto-18-sb">
       <div class="icon icon_sale"></div>
@@ -17,18 +24,17 @@ $color = get_sub_field('color') ?? false;
   <?php } ?>
 
   <div class="product-item-icon flex-c obj-contain">
-    <img src="<?php echo $img;  ?>" alt="">
+    <?php echo $product->get_image();?>
 
     <?php if ($color) { ?>
       <div class="product-item-circle" style="--circle: <?php echo $color ?>;"></div>
     <?php } ?>
   </div>
   <div class="product-item-info info flex h-between w-full">
-    <p><?php echo $title ?></p>
+    <p><?php echo get_the_title($product_id); ?></p>
     <div class="price">
       <p>
-        <?php echo $price ?>
-        <span>грн</span>
+        <?php echo $product->get_price_html(); ?>
       </p>
 
     </div>
