34ad2963d81d074d95e8c67a99d833061cd605f2
molinski <to.molinski@gmail.com>
2025-02-13 23:25:58 +0200

    new builder blocks

--- src/templates/components/grid-category.php
+++ src/templates/components/grid-category.php
@@ -1,22 +1,22 @@
 <?php
-$sale = get_sub_field('sale');
-$title = get_sub_field('title');
-$price = get_sub_field('price');
-$img = wp_get_attachment_image_url(get_sub_field('img'), 'full');
-$link = get_sub_field('link');
+$cat_id = get_sub_field('category');
 $type = get_row_layout();
-$color = get_sub_field('color') ?? false;
 $size = get_sub_field('size') ?? "";
+$color = get_field('product_color', 'product_cat_'.$cat_id);
+$img = get_field('photo', 'product_cat_'.$cat_id);
+
+$term = get_term($cat_id, 'product_cat' );
+$term_link = get_term_link( $term );
 ?>
 
-<a href="<?php echo $link ?>" class="product-item <?php echo $type, " ", $size ?><?php echo $color ? ' on-hover' : '' ?>">
+<a href="<?php echo $term_link ?>" class="product-item <?php echo $type, " ", $size ?><?php echo $color ? ' on-hover' : '' ?>">
   <div class="product-item-info info flex h-between w-full v-center">
-    <h2 class="roboto-38"><?php echo $title ?></h2>
+    <h2 class="roboto-38"><?php echo $term->name ?></h2>
     <div class="icon icon_arrow"></div>
   </div>
 
   <div class="product-item-icon flex-c obj-contain">
-    <img src="<?php echo $img;  ?>" alt="">
+    <?php if($img){?><img src="<?php echo $img;  ?>" alt=""><?php }?>
 
     <?php if ($color) { ?>
       <div class="product-item-circle" style="--circle: <?php echo $color ?>;"></div>
