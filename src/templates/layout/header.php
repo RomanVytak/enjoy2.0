bee48597ebb0dedc93c4130d2867b0fed8c3e5d7
Roman Maidan <romecom2210@gmail.com>
2025-03-10 20:10:29 +0200

    add mini cart

--- src/templates/layout/header.php
+++ src/templates/layout/header.php
@@ -5,11 +5,7 @@
       <div class="header-logo flex">
         <a href="<?php echo home_url(); ?>"><?php renderLogo() ?></a>
       </div>
-      <?php
-      wp_nav_menu(array(
-          'theme_location' => 'header_menu',
-      ));
-      ?>
+
       <label class="header-burger burger flex-c flex-v icon">
         <input type="checkbox" name="burger">
         <span></span>
@@ -18,30 +14,36 @@
       </label>
     </div>
 
+    <?php
+    wp_nav_menu(array(
+      'theme_location' => 'header_menu',
+      'container_class'      => 'header-menu',
+    ));
+    ?>
+
     <div class="header-right flex-c">
-      <?php $phone = get_field('phone', 'option');?>
-      <?php if(!empty($phone)){?><a href="<?php echo phoneToLink($phone) ?>" class="header-tell flex">
-        <div class="icon icon_tell"></div>
-        <span class="txt"><?php echo $phone;?></span>
-      </a><?php }?>
-      <div class="header--bag flex">
-        <?php if (!is_cart()) {
-          $total = WC()->cart->get_cart_total();
-          $cart_items = WC()->cart->get_cart();
-          $total_items = count($cart_items);
-        ?>
-          <button class="flex-c flex-v" data-toggle-cart>
-            <div class="relative flex">
-              <div class="icon large icon_cart" data-toggle-cart title="Кошик"></div>
-              <div class="header--bag-count flex-c font_body-s <?php echo $total_items > 0 ? "" : "flex-n" ?>" data-psd="<?php echo $total_items ?>">
+      <?php $phone = get_field('phone', 'option'); ?>
+      <?php if (!empty($phone)) { ?><a href="<?php echo phoneToLink($phone) ?>" class="header-tell flex">
+          <div class="icon icon_tell"></div>
+          <span class="txt"><?php echo $phone; ?></span>
+        </a><?php } ?>
+      <div class="header-bag flex">
+
+        <button class="flex-c flex-v" data-toggle-cart>
+          <div class="relative flex">
+            <div class="icon large icon_cart" title="Кошик"></div>
+
+            <?php if (!is_cart()) {
+              $cart_items = WC()->cart->get_cart();
+              $total_items = count($cart_items);
+            ?>
+              <div class="header-count flex-c <?php echo $total_items > 0 ? "" : "flex-n" ?>" data-psd="<?php echo $total_items ?>">
                 <?php echo $total_items ?>
               </div>
-            </div>
-            <p class="header--bag-price font_body-s <?php echo $total_items > 0 ? "" : "flex-n" ?>">
-              <?php echo $total ?>
-            </p>
-          </button>
-        <?php } ?>
+
+            <?php } ?>
+          </div>
+        </button>
 
       </div>
     </div>
