e4a8ea8b772bd21a24d7cf1ad01611dc42277580
Roman Vytak <r.vytak@dynatech.lv>
2025-04-06 00:33:26 +0300

    updates

--- src/templates/sections/default/content.php
+++ src/templates/sections/default/content.php
@@ -10,7 +10,7 @@
     <section class="single-page" data-page="<?php echo is_cart();  ?>">
       <div class="container">
 
-        <h1 class="roboto-48"><?php the_title(); ?></h1>
+        <!-- <h1 class="roboto-48">?php the_title(); ?></h1> -->
 
         <div class="content"><?php the_content(); ?></div>
 
