ca5f99ed49909b1c6b782075cf024be669b163ef
Roman Vytak <r.vytak@dynatech.lv>
2025-04-06 00:35:45 +0300

    updates

--- src/templates/sections/legal/content.php
+++ src/templates/sections/legal/content.php
@@ -1,7 +1,7 @@
 <section class="legal">
   <div class="container">
 
-    <h1 class="roboto-48"><?php the_title(); ?></h1>
+    <!-- <h1 class="roboto-48">?php the_title(); ?></h1> -->
 
     <div class="content"><?php the_content(); ?></div>
 
