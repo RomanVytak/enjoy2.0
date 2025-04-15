b0057f18ade775eef9e9af162696a88afece4187
Roman Vytak <r.vytak@dynatech.lv>
2025-04-15 23:17:33 +0300

    add WP_HOME and WP_SITEURL to wp_config

--- app/wp-config.php
+++ app/wp-config.php
@@ -24,6 +24,9 @@ if (file_exists(dirname(__FILE__) . '/local-config.php')) {
 	include(dirname(__FILE__) . '/local-config.php');
 }
 
+define('WP_HOME','http://localhost/');
+define('WP_SITEURL','http://localhost/');
+
 /**#@+
  * Authentication unique keys and salts.
  *
