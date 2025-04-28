806841c775227a771a9c23155bd4784ca98fcc79
molinski <to.molinski@gmail.com>
2025-04-28 16:40:54 +0300

    add text builder

--- src/templates/components/grid-text.php
+++ src/templates/components/grid-text.php
@@ -2,6 +2,7 @@
 $title = get_sub_field('title');
 $subtitle = get_sub_field('subtitle');
 $size = get_sub_field('size');
+$img = get_sub_field('img');
 $type = get_row_layout();
 ?>
 
@@ -9,6 +10,11 @@ $type = get_row_layout();
   <div class="text-content">
     <?php if ($title): ?>
       <h2 class="roboto-38"><?php echo $title ?></h2>
+    <?php endif; ?>
+    <?php if ($img): ?>
+      <div class="promo-icon flex">
+        <?php echo wp_get_attachment_image( $img, 'full' );?>
+      </div>
     <?php endif; ?>
     <p><?php echo $subtitle ?></p>
   </div>
