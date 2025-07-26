b463a5a5ee5462f3c204c752dd09594054b81dcc
Roman Maidan <romecom2210@gmail.com>
2025-07-26 13:52:00 +0300

    fix: default page form styles

--- src/templates/pages/default.php
+++ src/templates/pages/default.php
@@ -2,10 +2,16 @@
 
 get_header();
 
+global $post;
+$page_id = $post->ID;
+
 renderSection('default/content');
 renderSection('home/builder');
-renderSection('partnership');
-renderSection('partners');
-renderSection('cooperate');
+
+if ($page_id === 174) { // show only on Cooperation page
+  renderSection('partnership');
+  renderSection('partners');
+  renderSection('cooperate');
+}
 
 get_footer();
